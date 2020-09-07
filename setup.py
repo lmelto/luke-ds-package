ead58eacba2ba0ac7ac251faf546d622fcca7de6
Luke Melto <lukiepookieofficial@users.noreply.github.com>
2020-09-06 20:26:06 -0500

    created datetime function

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="luke-ds-package",
+    version="0.0.1",
+    author="Luke Melto",
+    author_email="lmelto@me.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lukiepookieofficial/luke-ds-package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU GPLv3 License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
